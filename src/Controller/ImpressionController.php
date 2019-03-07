1fb24ae137ba3e11865f656f2852ea968c8caf9b
kaiser tollo <tollosakho@gmail.com>
2019-03-07 15:00:47 +0000

    impression system working need to add connexion log

--- src/Controller/ImpressionController.php
+++ src/Controller/ImpressionController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\BilletPtb;
+use Symfony\Component\HttpFoundation\Response;
 
 class ImpressionController extends AbstractController
 {
@@ -16,4 +18,24 @@ class ImpressionController extends AbstractController
             'controller_name' => 'ImpressionController',
         ]);
     }
+
+    /**
+    * @Route("/blogs/{id}/{num}",name="blog_show")
+    */
+   public function show($id,$num)
+   {
+        $entityManager = $this->getDoctrine()->getManager();
+        $billet = $entityManager->getRepository(BilletPtb::class)->find($id);
+        $n = intval($num) ;
+        $array=array();
+        for( $i=0;$i < $n;$i++)
+        {
+            array_push($array, $billet->getNumeroDernierBillets()+$i);
+        }
+        $billet->setNumeroDernierBillets($array[$n-1]+1);
+        $entityManager->flush();
+        return $this->render('impression/index.html.twig', [
+            'billet' => $billet,'nbrebillet' => $array
+        ]);
+   }
 }
