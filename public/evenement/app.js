5946ee2a94ab0a8fd86689a36c6efd59f4962a49
THERA <ddthera@gmail.com>
2019-03-31 18:43:27 +0000

    first commit de event ajax

--- public/evenement/app.js
+++ public/evenement/app.js
@@ -1,14 +1,69 @@
 $(document).ready(function () {
+
+
+    $('.foSection').on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: $(this).attr('action'),
+            type: $(this).attr('method'),
+            data: $(this).serialize(),
+            dataType: 'json',
+            success: function (data) {
+                switch(data.status) {
+                    case 'error':
+                        swal("Erreur", data.message, "error");
+                        break;
+                    case 'success':
+                        swal("SUCCES", data.message, "success");
+                        break;
+                    default:
+                        swal("Error", data.message, "error");
+                }
+            },
+            error: function (error) {
+                alert('Erreur '+error);
+            }
+        });
+        return false;
+    });
+
+    $('.foTrajet').on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: $(this).attr('action'),
+            type: $(this).attr('method'),
+            data: $(this).serialize(),
+            dataType: 'json',
+            success: function (data) {
+                switch(data.status) {
+                    case 'error':
+                        console.log(data);
+                        swal("Erreur", data.message, "error");
+                        break;
+                    case 'success':
+                        swal("SUCCES", data.message, "success");
+                        break;
+                    default:
+                        swal("Error", data.message, "error");
+                }
+            },
+            error: function (error) {
+                alert('Erreur '+error);
+            }
+        });
+        return false;
+    });
+
+
     $('#btnSection').on('click', function (e) {
         e.preventDefault();
         var $formSection = $('#section');
         if ($formSection.css('display') === ('none')) {
 
-            $formSection.slideDown(1000);
-        }
-        else {
+            $formSection.slideToggle(1000);
+        } else {
 
-            $formSection.slideUp(1000);
+            $formSection.slideToggle(1000);
         }
     });
     $('#btnTrajet').on('click', function (e) {
@@ -16,11 +71,10 @@ $(document).ready(function () {
         var $formTrajet = $('#trajet');
         if ($formTrajet.css('display') === ('none')) {
 
-            $formTrajet.fadeIn(1000);
-        }
-        else {
+            $formTrajet.slideToggle(1000);
+        } else {
 
-            $formTrajet.fadeOut(1000);
+            $formTrajet.slideToggle(1000);
         }
     });
     $('#btnAddBilletEvent').on('click', function (e) {
@@ -30,10 +84,9 @@ $(document).ready(function () {
         if ($formBillet.css('display') === ('none')) {
 
             $formBillet.slideToggle(1000);
-        }
-        else {
+        } else {
 
-            $formBillet.fadeOut(1000);
+            $formBillet.slideToggle(1000);
         }
     })
 });
