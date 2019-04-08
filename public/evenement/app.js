5a61c73119b3ff729be520bf3d5227bd16b750c8
THERA <ddthera@gmail.com>
2019-04-08 11:00:04 +0000

    ajax avec abonnement mais on peut tjrs ajoute 2 abonnes avec le meme nom

--- public/evenement/app.js
+++ public/evenement/app.js
@@ -27,6 +27,33 @@ $(document).ready(function () {
         return false;
     });
 
+    //CREATION DE L'ABONNEE
+    $('.foAbonne').on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: $(this).attr('action'),
+            type: $(this).attr('method'),
+            data: $(this).serialize(),
+            dataType: 'json',
+            success: function (data) {
+                switch(data.status) {
+                    case 'error':
+                        swal("Erreur", data.message, "error");
+                        break;
+                    case 'success':
+                        swal("SUCCES", data.message, "success");
+                        break;
+                    default:
+                        swal("Errer", data.message, "error");
+                }
+            },
+            error: function (error) {
+                alert('Erreur '+error);
+            }
+        });
+        return false;
+    });
+
     $('.form_new_event').on('submit', function (e) {
         e.preventDefault();
         $.ajax({
